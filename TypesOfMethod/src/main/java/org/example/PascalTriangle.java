29fc12facbe24e6a561a0c40a2ce997e60e5850b
Ashutosh Yadav <ashutoshyadav9580@gmail.com>
2026-09-01 22:12:14 +0530

    Wave Printing

--- TypesOfMethod/src/main/java/org/example/PascalTriangle.java
+++ TypesOfMethod/src/main/java/org/example/PascalTriangle.java
@@ -1,4 +1,22 @@
 package org.example;
 
+import java.awt.*;
+import java.util.ArrayList;
+
 public class PascalTriangle {
+    public List<List<Integer>> generate(int n) {
+        List<List<Integer>> ans = new ArrayList<>();
+        for(int i=0 ; i<n ; i++){
+            List<Integer> l = new ArrayList<>();
+            for(int j = 0 ; j<=i ; j++){
+                if(j==0 || j==i) l.add(1);
+                else{
+                    l.add(((ArrayList<?>) ans).get(i-1).get(j) + ((ArrayList<?>) ans).get(i-1).get(j-1));
+                }
+            }
+            ans.add(l);
+        }
+        return ans;
+    }
 }
+
