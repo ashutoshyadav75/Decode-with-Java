2a1143b78a39d3914d1e5b878ea749bab88b88ce
Ashutosh Yadav <ashutoshyadav9580@gmail.com>
2026-09-03 22:18:59 +0530

    Eqaulity Operator

---
+++ TypesOfMethod/src/main/java/org/example/EqualsAndDouble.java
@@ -0,0 +1,15 @@
+package org.example;
+
+public class EqualsAndDouble {
+    public static void main(String[] args) {
+        String s = "abcdxyz";
+        String t = "abcxyz";
+        System.out.println(s==t);
+        String t = "abc";
+        t = t + "xyz";
+        System.out.println(s==t);
+        String b = new String(s);
+        System.out.println(s==t);
+
+    }
+}
