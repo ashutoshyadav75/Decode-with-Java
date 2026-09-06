eeed97f6dc5366a910562ce70db4e71dd399a5f6
Ashutosh Yadav <ashutoshyadav9580@gmail.com>
2026-09-06 22:05:21 +0530

    four

--- TypesOfMethod/src/main/java/org/example/FourSome.java
+++ TypesOfMethod/src/main/java/org/example/FourSome.java
@@ -16,9 +16,14 @@ public class FourSome {
                     continue;
                 }
                 for(int j=i+1;j<nums.length-2;j++){
-                    if(j>i+1&&nums[j]==nums[j-1]){
-                        continue;
-                    }
+                    if(j>i+1&&nums[j]==nums[j-1])for(int i=0;i<nums.length-3;i++){
+                        if(i>0&&nums[i]==nums[i-1]){
+                            continue;
+                        }
+                        for(int j=i+1;j<nums.length-2;j++){
+                            if(j>i+1&&nums[j]==nums[j-1]){
+                                continue;
+                            }
                     int left=j+1; int right=nums.length-1;
                     while(left<right){
                         long sum=(long)nums[i]+nums[j]+nums[left]+nums[right];
